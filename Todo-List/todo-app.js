447fa20260669fea13827c189b41d8c91d5a4f24
Danko Reno <oversmile13@gmail.com>
2023-08-27 08:59:25 +0600

    Initial commit

---
+++ Todo-List/todo-app.js
@@ -0,0 +1,102 @@
+(function(){
+    function CreateAppTitle(title){
+        let appTitle = document.createElement('h2');
+        appTitle.innerHTML = title;
+        return appTitle;
+    };
+
+    function CreateTodoItemForm(){
+        let form = document.createElement('form');
+        let input = document.createElement('input');
+        let buttonWrapper = document.createElement('div');
+        let button = document.createElement('button');
+
+        form.classList.add('input-group', 'mb-3');
+        input.classList.add('form-control');
+        input.placeholder ='Enter the name of the new case';
+        buttonWrapper.classList.add('input-group-append');
+        button.classList.add('btn', 'btn-primary');
+        button.textContent = 'Add case';
+
+        buttonWrapper.append(button);
+        form.append(input);
+        form.append(buttonWrapper);
+
+        return{
+            form,
+            input,
+            button,
+        };
+    };
+
+    function CreateTodoList(){
+        let list = document.createElement('li');
+        list.classList.add('list-group');
+        return list;
+    };
+
+    function CreateTodoItem(name){
+        let item = document.createElement('ol');
+        let buttonGroup = document.createElement('div');
+        let doneButton = document.createElement('button');
+        let deleteButton = document.createElement('button');
+
+        item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+        item.textContent = name;
+
+        buttonGroup.classList.add('btn-group', 'btn-groip-sm');
+        doneButton.classList.add('btn', 'btn-success');
+        doneButton.textContent = 'Done';
+        deleteButton.classList.add('btn', 'btn-danger');
+        deleteButton.textContent = 'Delete';
+
+        buttonGroup.append(doneButton);
+        buttonGroup.append(deleteButton);
+        item.append(buttonGroup);
+
+        return{
+            item,
+            doneButton,
+            deleteButton,
+        }
+    };
+
+    function CreateTodoApp(container, title = 'Todo list'){
+
+        let todoAppTitle = CreateAppTitle(title);
+        let todoItemForm = CreateTodoItemForm();
+        let todoList = CreateTodoList();
+
+        container.append(todoAppTitle);
+        container.append(todoItemForm.form);
+        container.append(todoList);
+
+        todoItemForm.form.addEventListener('submit', function(e){
+            e.preventDefault();
+
+            if(!todoItemForm.input.value){
+                console.log('Empty input field');
+                return;
+            }
+
+
+            let todoItem = CreateTodoItem(todoItemForm.input.value);
+            
+            todoItem.doneButton.addEventListener('click', function(){
+                todoItem.item.classList.toggle('list-group-item-success');
+            });
+            todoItem.deleteButton.addEventListener('click', function(){
+                if(confirm('Are you sure?')){
+                    todoItem.item.remove();
+                }
+            });
+
+            todoList.append(todoItem.item);
+            
+            todoItemForm.input.value='';
+
+        });
+    };
+
+    window.CreateTodoApp = CreateTodoApp;
+})();
